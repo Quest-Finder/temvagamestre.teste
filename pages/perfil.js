ea8dcf4f4be93ecebfa743f09a1e86daa0d7a972
Renata Valois <renata.rsv9@gmail.com>
2024-05-16 17:51:21 -0300

    Atualização da constante PATH, adição de novos objetos e organização dos testes

--- pages/perfil.js
+++ pages/perfil.js
@@ -12,12 +12,13 @@ exports.PerfilPage = class PerfilPage {
 
   async clicarExtenderBio() {
     const extenderBioButton = "//button[contains(.,'Ver mais')]";
-    await this.page.click(extenderBioButton);
+    await this.page.click(extenderBioButton);    
   }
 
   async clicarReduzirBio() {
     const reduzirBioButton = "//button[contains(.,'Ver menos')]";
     await this.page.click(reduzirBioButton);
+       
   }
 
   async validarTitulo() {
@@ -32,4 +33,35 @@ exports.PerfilPage = class PerfilPage {
     const conquistas = "(//h1[contains(.,'Conquistas')])[1]";
     await expect(this.page.locator(conquistas)).toBeVisible();
   }
+
+  async validarNome() {
+    const nomeUsuario = "//h2[contains(.,'Lucas Marcelo')]";
+    await expect(this.page.locator(nomeUsuario)).toBeVisible();
+  }
+
+  async validarPronome() {
+    const pronomeUsuario = "//h2[contains(.,'(Ele - Dele)')]";
+    await expect(this.page.locator(pronomeUsuario)).toBeVisible();
+  }
+  
+  async validarUsername() {
+    const username = "//h2[contains(.,'@mestremarcelo')]";
+    await expect(this.page.locator(username)).toBeVisible();
+  }
+
+  async validarUserTitle() {
+    const userTitle = "//h2[contains(.,'Mestre D&D com 15 anos de experiência')]";
+    await expect(this.page.locator(userTitle)).toBeVisible();
+  }
+
+  async validarLocalResidencia() {
+    const localResidencia = "//h3[contains(.,'São Paulo | Brasil')]";
+    await expect(this.page.locator(localResidencia)).toBeVisible();
+  }
+
+  async validarIdade() {
+    const idadeUsuario = "//h2[contains(.,'35 anos')]";
+    await expect(this.page.locator(idadeUsuario)).toBeVisible();
+  }
+
 };
--- tests/perfil.spec.js
+++ tests/perfil.spec.js
@@ -1,12 +1,37 @@
 const { test } = require('@playwright/test');
 import { PerfilPage } from '../pages/perfil.js';
 
+
 test('Acessar perfil', async ({ page }) => {
+
   const perfilPage = new PerfilPage(page);
   await perfilPage.goto();
   await perfilPage.validarTitulo();
-  await perfilPage.clicarExtenderBio();
-  await perfilPage.clicarReduzirBio();
+});
+
+test('Validar dados do usuário', async ({ page }) => {
+  const perfilPage = new PerfilPage(page);
+  await perfilPage.goto();
+  await perfilPage.validarNome();
+  await perfilPage.validarPronome();
+  await perfilPage.validarUsername();
+  await perfilPage.validarUserTitle();
+  await perfilPage.validarLocalResidencia();
+  await perfilPage.validarIdade();
+});
+
+test('Validar Estilos de jogo e Conquistas', async ({ page }) => {
+  const perfilPage = new PerfilPage(page);
+  await perfilPage.goto();
   await perfilPage.validarEstilosPreferidos();
   await perfilPage.validarConquistas();
+
+});
+
+test('Validar Bio', async ({ page }) => {
+  const perfilPage = new PerfilPage(page);
+  await perfilPage.goto();
+  await perfilPage.clicarExtenderBio();
+  await perfilPage.clicarReduzirBio();
+  
 });
