f49abf0548cd7e5df5a8a4c79c669f3b9dc55de3
Renata Valois <renata.rsv9@gmail.com>
2024-08-09 13:51:13 -0300

    fix variável de ambiente

--- playwright.config.js
+++ playwright.config.js
@@ -26,8 +26,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.URL,
-
+    baseURL: process.env.BASEURL || 'https://homolog.temvagamestre.com/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
