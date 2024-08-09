930f679c4fb4133ddf7998c46f1e6fbc26e71c43
brunomachadors <brunomachadors@gmail.com>
2024-08-09 18:08:36 +0100

    Updating env variables

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,6 @@ require('dotenv').config();
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -26,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASEURL,
+    baseURL: process.env.BASE_URL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
